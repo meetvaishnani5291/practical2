05838d2e31ef62db1adaea7d3191c8a442d3d884
meetvaishnani5291 <meet.vaishnani@simformsolutions.com>
2023-03-21 10:11:43 +0530

    phase 3 implemented

--- index.js
+++ index.js
@@ -84,6 +84,52 @@ const getShopStatus = function (dayString, timeString, phase) {
       );
     }
   } else if (phase === "phase3") {
+    if (
+      shopScheduleForDay !== undefined &&
+      compareTime(shopScheduleForDay.open, time) <= 0 &&
+      compareTime(shopScheduleForDay.close, time) >= 0
+    ) {
+      let hours = compareTime(shopScheduleForDay.close, time);
+      console.log(
+        `Open, The shop will be closed within ${hours.toFixed(2)} Hrs`
+      );
+    } else {
+      let hours;
+      if (
+        shopScheduleForDay !== undefined &&
+        compareTime(shopScheduleForDay.open, time) > 0
+      ) {
+        hours = compareTime(shopScheduleForDay.open, time);
+      } else {
+        let nextOpenDay = day + 1;
+        for (let i = 0; i < 7; i++) {
+          if (shopSchedule[nextOpenDay % 7] !== undefined) break;
+          nextOpenDay++;
+        }
+        console.log(nextOpenDay);
+        hours =
+          compareTime(shopSchedule[nextOpenDay % 7].open, time) +
+          Math.abs(nextOpenDay - day) * 24;
+      }
+      const days = parseInt(hours / 24);
+      if (days === 0) {
+        console.log(
+          `Shop is Currently Closed. and it will be open after ${hours} Hrs`
+        );
+      } else if (days === 1) {
+        console.log(
+          `Shop is Currently Closed. and it will be open after ${days} Day and ${
+            hours % 24
+          } Hrs`
+        );
+      } else {
+        console.log(
+          `Shop is Currently Closed. and it will be open after ${days} Days and ${(
+            hours % 24
+          ).toFixed(2)} Hrs`
+        );
+      }
+    }
   }
 };
 
